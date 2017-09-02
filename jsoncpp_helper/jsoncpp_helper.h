73ca22a5e497adda22de510fc6076556041c5bcc
zhezhao <zhezhaoxu@gmail.com>
2017-09-02 20:31:07 +0800

    update

--- jsoncpp_helper/jsoncpp_helper.h
+++ jsoncpp_helper/jsoncpp_helper.h
@@ -2,7 +2,7 @@
  @Author: zhezhaoxu
  @Created Time : 2017年08月31日 星期四 22时03分51秒
  @File Name: jsoncpp_helper.h
- @Description: 
+ @Description:
  提供如下功能
  1. 方便检查 jsoncpp Json::Value 对象的字段类型
  ******************************************************/
@@ -41,43 +41,130 @@ const struct string_tag_t string_tag;
 const struct array_tag_t array_tag;
 const struct object_tag_t object_tag;
 
-bool __checkJsoncppArgs(const Json::Value&) { return true; }
+// base
+bool __checkJsonArgs(const Json::Value&) { return true; }
 
-#define DEFINE_checkJsoncppArgs(type1, type2)                                 \
+#define DEFINE_checkJsonArgs(type1, type2)                                 \
 	template <typename... Args>                                               \
-	bool __checkJsoncppArgs(const Json::Value& value, const char* key, type1, \
+	bool __checkJsonArgs(const Json::Value& value, const char* key, type1, \
 							Args... args) {                                   \
 		if (!(value.isMember(key) && value[key].type2())) {                   \
 			return false;                                                     \
 		}                                                                     \
-		return __checkJsoncppArgs(value, args...);                            \
+		return __checkJsonArgs(value, args...);                            \
 	}
 
-DEFINE_checkJsoncppArgs(null_tag_t, isNull);
-DEFINE_checkJsoncppArgs(bool_tag_t, isBool);
-DEFINE_checkJsoncppArgs(int32_tag_t, isInt);
-DEFINE_checkJsoncppArgs(int64_tag_t, isInt64);
-DEFINE_checkJsoncppArgs(uint32_tag_t, isUInt);
-DEFINE_checkJsoncppArgs(uint64_tag_t, isUInt64);
-DEFINE_checkJsoncppArgs(integral_tag_t, isIntegral);
-DEFINE_checkJsoncppArgs(double_tag_t, isDouble);
-DEFINE_checkJsoncppArgs(numeric_tag_t, isNumeric);
-DEFINE_checkJsoncppArgs(string_tag_t, isString);
-DEFINE_checkJsoncppArgs(array_tag_t, isArray);
-DEFINE_checkJsoncppArgs(object_tag_t, isObject);
+DEFINE_checkJsonArgs(null_tag_t, isNull);
+DEFINE_checkJsonArgs(bool_tag_t, isBool);
+DEFINE_checkJsonArgs(int32_tag_t, isInt);
+DEFINE_checkJsonArgs(int64_tag_t, isInt64);
+DEFINE_checkJsonArgs(uint32_tag_t, isUInt);
+DEFINE_checkJsonArgs(uint64_tag_t, isUInt64);
+DEFINE_checkJsonArgs(integral_tag_t, isIntegral);
+DEFINE_checkJsonArgs(double_tag_t, isDouble);
+DEFINE_checkJsonArgs(numeric_tag_t, isNumeric);
+DEFINE_checkJsonArgs(string_tag_t, isString);
+DEFINE_checkJsonArgs(array_tag_t, isArray);
+DEFINE_checkJsonArgs(object_tag_t, isObject);
+
+template <typename T>
+struct is_jsontype {
+	enum { value = 0 };
+};
+template <>
+struct is_jsontype<bool> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<int32_t> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<uint32_t> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<int64_t> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<uint64_t> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<double> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<const char*> {
+	enum { value = 1 };
+};
+template <>
+struct is_jsontype<Json::Value> {
+	enum { value = 1 };
+};
+
+// base
+void __setJsonValue(Json::Value&) {}
+
+template <typename T, typename... Args,
+		  typename = typename std::enable_if<is_jsontype<
+			  typename std::remove_reference<T>::type>::value>::type>
+void __setJsonValue(Json::Value& value, const char* key, T&& val,
+					   Args... args) {
+	value[key] = std::forward<T>(val);
+	__setJsonValue(value, args...);
+}
+
+// base
+void __setJsonArrayValue(Json::Value&) {}
+
+template <typename T, typename... Args,
+		  typename = typename std::enable_if<is_jsontype<
+			  typename std::remove_reference<T>::type>::value>::type>
+void __setJsonArrayValue(Json::Value& arrayValue, T&& val, Args... args) {
+	arrayValue[ arrayValue.size() ] = std::forward<T>(val);
+	__setJsonArrayValue(arrayValue, args...);
+}
+
+// 利用 SFINAE
+template <typename T>
+void __setJsonArrayValue(Json::Value& arrayValue, T input, size_t N) {
+	for (Json::ArrayIndex index = 0; index < N; ++index) {
+		arrayValue[index] = input[index];
+	}
+}
 
 /********* API ****************************/
 
 // 检查value中字段类型, 使用方法如下
-// checkJsoncppArgs(value, key1, int32_tag, key2, string_tag);
+// checkJsonArgs(value, key1, int32_tag, key2, string_tag);
 template <typename... Args>
-bool checkJsoncppArgs(const Json::Value& value, Args... args) {
-	return __checkJsoncppArgs(value, args...);
+bool checkJsonArgs(const Json::Value& value, Args... args) {
+	return __checkJsonArgs(value, args...);
+}
+
+// 设置 value 中的字段，使用方法如下：
+// setJsonValue(value, key1, val1, key2, val2, key3, val3, ...);
+// key* 为字符串类型， val* 为 jsoncpp 支持的任意类型
+template <typename... Args>
+void setJsonValue(Json::Value& value, Args... args) {
+	__setJsonValue(value, args...);
 }
 
 
-/******** end API ***********************/
+// 设置数组类型的值，使用方法如下：
+// 有两种形式：
+// 1. setJsonArrayValue(arrayValue, val1, val2, val3, ...);
+//  val* 为 jsoncpp 支持的任意类型
+// 2. setJsonArrayValue(arrayValue, input, N);
+//  input 类型只要重载了 [] 操作符就行，N 为数组大小
+template <typename... Args>
+void setJsonArrayValue(Json::Value& arrayValue, Args... args) {
+	__setJsonArrayValue(arrayValue, args...);
+}
 
+/******** end API ***********************/
 }
 }
 
--- jsoncpp_helper/test/jsoncpp_helper_test.cpp
+++ jsoncpp_helper/test/jsoncpp_helper_test.cpp
@@ -9,55 +9,55 @@
 #include "gtest/gtest.h"
 #include "json/json.h"
 
-TEST(JSONCPP_HELPER, basic) {
+TEST(JSONCPP_HELPER, check_basic) {
 	using namespace utils::jsoncpp_helper;
 	Json::Value value;
-	ASSERT_TRUE(!checkJsoncppArgs(value, "id", int32_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "id", int32_tag));
 	value["id"] = 158;
-	ASSERT_TRUE(checkJsoncppArgs(value, "id", int32_tag));
-	ASSERT_TRUE(checkJsoncppArgs(value, "id", uint32_tag));
-	ASSERT_TRUE(checkJsoncppArgs(value, "id", int64_tag));
-	ASSERT_TRUE(checkJsoncppArgs(value, "id", uint64_tag));
-	ASSERT_TRUE(checkJsoncppArgs(value, "id", numeric_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "id", int32_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "id", uint32_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "id", int64_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "id", uint64_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "id", numeric_tag));
 
-	ASSERT_TRUE(!checkJsoncppArgs(value, "name", string_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "name", string_tag));
 	value["name"] = "zhezhaoxu";
-	ASSERT_TRUE(checkJsoncppArgs(value, "name", string_tag));
-	ASSERT_TRUE(!checkJsoncppArgs(value, "name", numeric_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "name", string_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "name", numeric_tag));
 
-	ASSERT_TRUE(!checkJsoncppArgs(value, "isgood", bool_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "isgood", bool_tag));
 	value["isgood"] = true;
-	ASSERT_TRUE(checkJsoncppArgs(value, "isgood", bool_tag));
-	ASSERT_TRUE(!checkJsoncppArgs(value, "isgood", int32_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "isgood", bool_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "isgood", int32_tag));
 
-	ASSERT_TRUE(!checkJsoncppArgs(value, "score", double_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "score", double_tag));
 	value["score"] = 3.0;
-	ASSERT_TRUE(checkJsoncppArgs(value, "score", double_tag));
-	ASSERT_TRUE(checkJsoncppArgs(value, "score", int32_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "score", double_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "score", int32_tag));
 
 	value["score"] = 3.2;
-	ASSERT_TRUE(checkJsoncppArgs(value, "score", double_tag));
-	ASSERT_TRUE(!checkJsoncppArgs(value, "score", int32_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "score", double_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "score", int32_tag));
 
-	ASSERT_TRUE(!checkJsoncppArgs(value, "cars", array_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "cars", array_tag));
 	value["cars"] = "bwm";
-	ASSERT_TRUE(!checkJsoncppArgs(value, "cars", array_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "cars", array_tag));
 	value["cars"] = Json::arrayValue;
-	ASSERT_TRUE(checkJsoncppArgs(value, "cars", array_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "cars", array_tag));
 	value["cars"][0] = "bwm";
-	ASSERT_TRUE(checkJsoncppArgs(value, "cars", array_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "cars", array_tag));
 	value["cars"][1] = "benz";
-	ASSERT_TRUE(checkJsoncppArgs(value, "cars", array_tag));
-	ASSERT_TRUE(!checkJsoncppArgs(value, "cars", object_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "cars", array_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "cars", object_tag));
 
 	value["feed"] = Json::objectValue;
-	ASSERT_TRUE(checkJsoncppArgs(value, "feed", object_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "feed", object_tag));
 	value["feed"]["title"] = "let's go";
-	ASSERT_TRUE(checkJsoncppArgs(value, "feed", object_tag));
-	ASSERT_TRUE(!checkJsoncppArgs(value, "feed", array_tag));
+	ASSERT_TRUE(checkJsonArgs(value, "feed", object_tag));
+	ASSERT_TRUE(!checkJsonArgs(value, "feed", array_tag));
 }
 
-TEST(JSONCPP_HELPER, advanced) {
+TEST(JSONCPP_HELPER, check_advanced) {
 	using namespace utils::jsoncpp_helper;
 	Json::Value value;
 	value["id"] = 158;
@@ -69,9 +69,74 @@ TEST(JSONCPP_HELPER, advanced) {
 	value["list"][1] = 2;
 	value["obj"]["home"] = "zhangshu";
 
-	assert(checkJsoncppArgs(value, "id", int32_tag, "qq", uint32_tag, "wechat",
-							string_tag, "null", null_tag, "score", double_tag,
-							"list", array_tag, "obj", object_tag));
+	assert(checkJsonArgs(value, "id", int32_tag, "qq", uint32_tag, "wechat",
+						 string_tag, "null", null_tag, "score", double_tag,
+						 "list", array_tag, "obj", object_tag));
 
-	assert(!checkJsoncppArgs(value, "addr", string_tag));
+	assert(!checkJsonArgs(value, "addr", string_tag));
+}
+
+TEST(JSONCPP_HELPER, setvalue_basic) {
+	using namespace utils::jsoncpp_helper;
+	Json::Value value;
+	setJsonValue(value, "name", "zhezhaoxu");
+	ASSERT_TRUE(checkJsonArgs(value, "name", string_tag));
+	ASSERT_TRUE(value["name"] == "zhezhaoxu");
+
+	setJsonValue(value, "id", 158);
+	ASSERT_TRUE(checkJsonArgs(value, "id", int32_tag));
+	ASSERT_TRUE(value["id"] == 158);
+
+	setJsonValue(value, "good", true);
+	ASSERT_TRUE(checkJsonArgs(value, "good", bool_tag));
+	ASSERT_TRUE(value["good"] == true);
+
+	setJsonValue(value, "score", 95.5);
+	ASSERT_TRUE(checkJsonArgs(value, "score", double_tag));
+	ASSERT_TRUE(value["score"] == 95.5);
+
+	Json::Value subvalue;
+	subvalue["name"] = "zs";
+	subvalue["id"] = 10086;
+	subvalue["addr"] = "zhangshu";
+	setJsonValue(value, "school", subvalue);
+	ASSERT_TRUE(checkJsonArgs(value, "school", object_tag));
+	ASSERT_TRUE(value["school"] == subvalue);
+}
+
+TEST(JSONCPP_HELPER, setarrayValue_basic) {
+	using namespace utils::jsoncpp_helper;
+	Json::Value value;
+	setJsonArrayValue(value["array"], 158, 19930326, "zhezhao", true, 95.5);
+	checkJsonArgs(value, "array", array_tag);
+	checkJsonArgs(value["array"][0] == 158);
+	checkJsonArgs(value["array"][1] == 19930326);
+	checkJsonArgs(value["array"][2] == "zhezhao");
+	checkJsonArgs(value["array"][3] == true);
+	checkJsonArgs(value["array"][4] == 95.5);
+
+	std::vector<int> tickets = {1, 2, 3};
+	setJsonArrayValue(value["tickets"], tickets, 3);
+	checkJsonArgs(value, "tickets", array_tag);
+	EXPECT_TRUE(value["tickets"][0] == 1);
+	EXPECT_TRUE(value["tickets"][1] == 2);
+	EXPECT_TRUE(value["tickets"][2] == 3);
+
+	setJsonArrayValue(value["info"], "zhezhaoxu", 24);
+	checkJsonArgs(value, "info", array_tag);
+	EXPECT_TRUE(value["info"][0] == "zhezhaoxu");
+	EXPECT_TRUE(value["info"][1] == 24);
+}
+
+TEST(JSONCPP_HELPER, setvalue_advanced) {
+	using namespace utils::jsoncpp_helper;
+	Json::Value value;
+	setJsonValue(value, "name", "zhezhaoxu", "id", 158, "good", true, "score",
+				 95.5);
+	ASSERT_TRUE(checkJsonArgs(value, "name", string_tag, "id", int32_tag,
+							  "good", bool_tag, "score", double_tag));
+	ASSERT_TRUE(value["name"] == "zhezhaoxu");
+	ASSERT_TRUE(value["id"] == 158);
+	ASSERT_TRUE(value["good"] == true);
+	ASSERT_TRUE(value["score"] == 95.5);
 }
