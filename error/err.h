312e85039003527223e03217b72c4effc143cd6e
zhezhao <zhezhaoxu@gmail.com>
2017-09-19 19:57:36 +0800

    add mutex/

---
+++ error/err.h
@@ -0,0 +1,55 @@
+/*******************************************************
+ @Author: zhezhaoxu
+ @Created Time : Tue 19 Sep 2017 07:45:44 PM CST
+ @File Name: err.h
+ @Description: from libzmq/src/err.hpp
+ ******************************************************/
+
+#ifndef UTILS_MUTEX_ERR_H_
+#define UTILS_MUTEX_ERR_H_
+
+#include "likely/likely.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// TODO abort() 之前打印调用栈信息
+
+//  Provides convenient way to check for POSIX errors.
+#define posix_assert(x)                                                  \
+	do {                                                                 \
+		if (unlikely(x)) {                                               \
+			const char *errstr = strerror(x);                            \
+			fprintf(stderr, "%s (%s:%d)\n", errstr, __FILE__, __LINE__); \
+			fflush(stderr);                                              \
+			abort();                                                     \
+		}                                                                \
+	} while (false)
+
+//  Provides convenient way to check for errors from getaddrinfo.
+#define gai_assert(x)                                                    \
+	do {                                                                 \
+		if (unlikely(x)) {                                               \
+			const char *errstr = gai_strerror(x);                        \
+			fprintf(stderr, "%s (%s:%d)\n", errstr, __FILE__, __LINE__); \
+			fflush(stderr);                                              \
+			abort();                                                     \
+		}                                                                \
+	} while (false)
+
+//  Provides convenient way to check whether memory allocation have succeeded.
+#define alloc_assert(x)                                                       \
+	do {                                                                      \
+		if (unlikely(!x)) {                                                   \
+			fprintf(stderr, "FATAL ERROR: OUT OF MEMORY (%s:%d)\n", __FILE__, \
+					__LINE__);                                                \
+			fflush(stderr);                                                   \
+			abort();                                                          \
+		}                                                                     \
+	} while (false)
+
+#endif
---
+++ mutex/mutex.h
@@ -0,0 +1,95 @@
+/*******************************************************
+ @Author: zhezhaoxu
+ @Created Time : Tue 19 Sep 2017 07:42:57 PM CST
+ @File Name: mutex.h
+ @Description: from libzmq/src/mutex.hpp
+ ******************************************************/
+
+#include "error/err.h"
+
+#include <pthread.h>
+
+namespace utils {
+
+class mutex_t {
+public:
+	inline mutex_t() {
+		int rc = pthread_mutexattr_init(&attr);
+		posix_assert(rc);
+
+		rc = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+		posix_assert(rc);
+
+		rc = pthread_mutex_init(&mutex, &attr);
+		posix_assert(rc);
+	}
+
+	inline ~mutex_t() {
+		int rc = pthread_mutex_destroy(&mutex);
+		posix_assert(rc);
+
+		rc = pthread_mutexattr_destroy(&attr);
+		posix_assert(rc);
+	}
+
+	inline void lock() {
+		int rc = pthread_mutex_lock(&mutex);
+		posix_assert(rc);
+	}
+
+	inline bool try_lock() {
+		int rc = pthread_mutex_trylock(&mutex);
+		if (rc == EBUSY) return false;
+
+		posix_assert(rc);
+		return true;
+	}
+
+	inline void unlock() {
+		int rc = pthread_mutex_unlock(&mutex);
+		posix_assert(rc);
+	}
+
+	inline pthread_mutex_t* get_mutex() { return &mutex; }
+
+private:
+	pthread_mutex_t mutex;
+	pthread_mutexattr_t attr;
+
+	// Disable copy construction and assignment.
+	mutex_t(const mutex_t&);
+	const mutex_t& operator=(const mutex_t&);
+};
+}
+
+namespace utils {
+struct scoped_lock_t {
+	scoped_lock_t(mutex_t& mutex_) : mutex(mutex_) { mutex.lock(); }
+
+	~scoped_lock_t() { mutex.unlock(); }
+
+private:
+	mutex_t& mutex;
+
+	// Disable copy construction and assignment.
+	scoped_lock_t(const scoped_lock_t&);
+	const scoped_lock_t& operator=(const scoped_lock_t&);
+};
+
+struct scoped_optional_lock_t {
+	scoped_optional_lock_t(mutex_t* mutex_) : mutex(mutex_) {
+		if (mutex != NULL) mutex->lock();
+	}
+
+	~scoped_optional_lock_t() {
+		if (mutex != NULL) mutex->unlock();
+	}
+
+private:
+	mutex_t* mutex;
+
+	// Disable copy construction and assignment.
+	scoped_optional_lock_t(const scoped_lock_t&);
+	const scoped_optional_lock_t& operator=(const scoped_lock_t&);
+};
+}
