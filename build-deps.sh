e9a83e32893d4b7444af9eaeb0ac3a5b30e2a55d
zhezhao <zhezhaoxu@gmail.com>
2017-10-28 23:49:30 +0800

    add build-deps.sh

---
+++ build-deps.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+echo "building deps/lua-5.3.4 ..."
+pre_dir=`pwd`
+cd deps/lua-5.3.4 && mkdir -p build && cd build && cmake .. && make
+cd $pre_dir
+
+echo "building deps/lua ..."
+pre_dir=`pwd`
+cd deps/lua && make CC=gcc
+cd $pre_dir
+
+echo "building deps/gflags ..."
+pre_dir=`pwd`
+cd deps/gflags && mkdir -p build && cd build && cmake .. && make
+cd $pre_dir
+
+echo "building deps/googletest ..."
+pre_dir=`pwd`
+cd deps/googletest && mkdir -p build && cd build && cmake .. && make
+cd $pre_dir
+
+echo "building deps/libzmq ..."
+pre_dir=`pwd`
+cd deps/libzmq && mkdir -p build && cd build && cmake .. && make
+cd $pre_dir
+
+echo "building deps/protobuf ..."
+pre_dir=`pwd`
+cd deps/protobuf && ./autogen.sh && ./configure && make
+cd $pre_dir
+
+echo "building deps/glog ..."
+pre_dir=`pwd`
+cd deps/glog && ./autogen.sh && ./configure && make
+cd $pre_dir
+
+echo "building deps/jsoncpp ..."
+pre_dir=`pwd`
+cd deps/jsoncpp && python amalgamate.py
+cd $pre_dir
